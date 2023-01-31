2adf69c5dd10048da765cd1316783d495865498f
Batı Gökovalı <batigokovali@icloud.com>
2023-01-31 17:57:18 +0300

    finished

---
+++ d2-hw/src/components/MyJumbotron.jsx
@@ -0,0 +1,17 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+function MyJumbotron() {
+  return (
+    <Card className="bg-image">
+      <Card.Body className='card-center'>
+        <Card.Title>Welcome to EpiBooks!</Card.Title>
+        <Card.Text>
+          Here you will find plenty of books!
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default MyJumbotron;
